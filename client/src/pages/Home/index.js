4b80151491627bf32e9d9caaed6945ae579934f2
Mateo Gabriel Rios <mateo903@gmail.com>
2021-05-09 20:40:27 -0300

    Creating Home Page

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import {APIURL} from '../../Constants'
+import Balance from '../../components/Balance'
+import PostFormCard from '../../components/PostFormCard'
+import ShowRecords from '../../components/ShowRecords'
+import './style.css'
+
+const Home = () => {
+  const [consulta, setConsulta] = useState({type:'GET', url:`${APIURL}/lasttenrecords/`, update: true})
+
+  const handleUpdate = () => {
+    setConsulta(({type:'GET', url: `${APIURL}/lasttenrecords/`, update: !consulta.update}))
+  }
+
+  return (
+    <div className='home'>
+      <div className='balance-container' >
+        <label>BALANCE</label>
+        <Balance update={consulta.update}/>
+      </div>
+      <PostFormCard sendForm={handleUpdate}/>
+      <label>Ultimos 10 registros</label>
+      <div>
+      </div>
+      <div className='list-container'>
+        <ShowRecords  url={consulta.url} type={consulta.type} sendPut={handleUpdate}  update={consulta.update}/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
