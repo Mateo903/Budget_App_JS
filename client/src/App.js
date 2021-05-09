bfa0ba3dcc60b976819fc6644a9c77fe32be66d8
Mateo Gabriel Rios <mateo903@gmail.com>
2021-05-09 13:45:58 -0300

    basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from './pages/Home'
+import AllRecords from './pages/AllRecords';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+
+
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/allrecords" component={AllRecords} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/AllRecords/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllRecords = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllRecords
