c84c6f9ab74e803460e8b236d0e28e06f59524ff
Mateo Gabriel Rios <mateo903@gmail.com>
2021-05-09 20:20:49 -0300

    Creating Post Form Component

---
+++ client/src/components/PostFormCard/index.js
@@ -0,0 +1,120 @@
+import React, {useState, useEffect} from 'react'
+import {formatDateInput, APIURL} from '../../Constants'
+import './style.css'
+
+import axios from 'axios'
+
+
+
+
+const PostFormCard = () => {
+
+  const [accessAPI, setAccessAPI] = useState({ type: 'GET', url: `${APIURL}/records/` });
+  const [bodyData, setbodyData] = useState({});
+
+  const [record, setRecord] = useState({
+    concept:'',
+    date:'',
+    amount:0,
+    type:1,
+  })
+
+  const {concept, date, amount, type} = record
+
+
+  const reqAPI = async() =>{
+
+    await axios({method: accessAPI.type, url: accessAPI.url, data: bodyData})
+
+  }
+
+
+  
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setRecord(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+
+  useEffect( () => {
+    setRecord(prevState => ({
+      ...prevState,
+      date: formatDateInput(new Date())
+    }))
+
+    reqAPI()
+  },[accessAPI.type, accessAPI.url, bodyData])
+
+  const handleSubmit = (e) =>{
+    
+    e.preventDefault()
+  }
+
+  const readForm = (newConcept,newDate,newAmount) => {
+    console.log(newConcept,newDate,newAmount)
+
+    setAccessAPI({type: 'POST', url:`${APIURL}/records/`})
+
+    setbodyData({
+      concept:newConcept,
+      date:newDate,
+      amount: newAmount,
+    })
+  }
+
+
+  return (
+    <form onSubmit={e => handleSubmit(e)} className='form-container'>
+      <div className='space'></div>
+      <div className='fields-container'>
+        <div className='concept-date-container'>
+          <input  className='concept-field'  
+                  maxLength='39' 
+                  name='concept' 
+                  onChange={handleChange} 
+                  placeholder='concepto' 
+                  type='text'
+           />
+          <input  className='date-field' 
+                  name='date' 
+                  onChange={handleChange} 
+                  type='date' 
+                  value={date}   
+          />
+
+        </div>
+        
+        <div className='amount-container'>
+          <select value={type} onChange={handleChange} name="type">
+
+            <option value={1}>ingreso</option>
+
+            <option value={-1}>egreso</option>
+          </select> 
+
+          <input  className='amount-field' 
+                  name='amount' 
+                  onChange={handleChange} 
+                  placeholder='monto' 
+                  type='number'  
+           />
+        </div>
+      </div>
+      <div className='buttons-container'>
+        <div  className='check' 
+              onClick={()=>readForm(concept, date, (amount*type))}
+              type='submit' 
+        >
+          <i className="fas fa-plus add"></i>
+        </div>
+      </div>
+
+    </form>
+  )
+}
+
+export default PostFormCard
