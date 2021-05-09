1f8c5f0823e275dd3dbd5a21849407f62fe59704
Mateo Gabriel Rios <mateo903@gmail.com>
2021-05-09 12:41:22 -0300

    create routes for manage the querys

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const mysql = require('mysql')
 const myconn = require('express-myconnection')
 const cors = require('cors')
 
+const routes = require('./routes')
+
 const app = express()
 app.set('port', process.env.PORT || 9000)
 const dbOptions = {
@@ -15,11 +17,16 @@ const dbOptions = {
 
 
 // middleware -----------------
+
 app.use(myconn(mysql, dbOptions, 'single'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors());
 
+
+//routes------------------
+
+app.use('/api', routes)
 
 // server running --------------------
 
